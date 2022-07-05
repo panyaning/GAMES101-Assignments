a59c0496aa19fad740d468c52acd6399cd88f4c3
潘亚宁 <82204137+panyaning@users.noreply.github.com>
2022-07-05 15:58:13 +0800

    Add files via upload
    Path Tracing

---
+++ Assignment7/PA7/Assignment7/Assignment7/build/CMakeFiles/RayTracing.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rosmelodic/桌面/games101_Assignments/Assignment7/PA7/Assignment7/Assignment7/BVH.cpp" "/home/rosmelodic/桌面/games101_Assignments/Assignment7/PA7/Assignment7/Assignment7/build/CMakeFiles/RayTracing.dir/BVH.cpp.o"
+  "/home/rosmelodic/桌面/games101_Assignments/Assignment7/PA7/Assignment7/Assignment7/Renderer.cpp" "/home/rosmelodic/桌面/games101_Assignments/Assignment7/PA7/Assignment7/Assignment7/build/CMakeFiles/RayTracing.dir/Renderer.cpp.o"
+  "/home/rosmelodic/桌面/games101_Assignments/Assignment7/PA7/Assignment7/Assignment7/Scene.cpp" "/home/rosmelodic/桌面/games101_Assignments/Assignment7/PA7/Assignment7/Assignment7/build/CMakeFiles/RayTracing.dir/Scene.cpp.o"
+  "/home/rosmelodic/桌面/games101_Assignments/Assignment7/PA7/Assignment7/Assignment7/Vector.cpp" "/home/rosmelodic/桌面/games101_Assignments/Assignment7/PA7/Assignment7/Assignment7/build/CMakeFiles/RayTracing.dir/Vector.cpp.o"
+  "/home/rosmelodic/桌面/games101_Assignments/Assignment7/PA7/Assignment7/Assignment7/main.cpp" "/home/rosmelodic/桌面/games101_Assignments/Assignment7/PA7/Assignment7/Assignment7/build/CMakeFiles/RayTracing.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
