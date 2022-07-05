25b66ec9fe69a7d85b1027a8892c33844964a875
潘亚宁 <82204137+panyaning@users.noreply.github.com>
2022-07-05 15:41:38 +0800

    Add files via upload
    Whitted Style Ray Tracing

---
+++ Assignment5/Code/build/CMakeFiles/RayTracing.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rosmelodic/桌面/games101_Assignments/Assignment5/Code/Renderer.cpp" "/home/rosmelodic/桌面/games101_Assignments/Assignment5/Code/build/CMakeFiles/RayTracing.dir/Renderer.cpp.o"
+  "/home/rosmelodic/桌面/games101_Assignments/Assignment5/Code/Scene.cpp" "/home/rosmelodic/桌面/games101_Assignments/Assignment5/Code/build/CMakeFiles/RayTracing.dir/Scene.cpp.o"
+  "/home/rosmelodic/桌面/games101_Assignments/Assignment5/Code/main.cpp" "/home/rosmelodic/桌面/games101_Assignments/Assignment5/Code/build/CMakeFiles/RayTracing.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
